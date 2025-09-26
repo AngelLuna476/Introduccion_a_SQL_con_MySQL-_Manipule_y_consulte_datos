938c39fea9bb82787dc0f089bb989ea574124f83
Angel Luna <joseangel.lunadelgado9910@gmail.com>
2025-09-26 14:04:15 -0600

    Update ejercicios.sql

--- ejercicios.sql
+++ ejercicios.sql
@@ -1,11 +1,3 @@
-# En esta aula te hablaremos sobre la historia de SQL y de My SQL.
-# También instalaremos Workbench para poder utilizar un IDE en el desarrollo de nuestros ejercicios.
-
-# Ya tu ambiente debe estar listo para trabajar. Te invito ver los videos y a ejecutar todos los 
-# comandos que vas a aprender.
-
-#Los comandos vistos en el aula anterior son los siguientes:
-
 USE world;
 
 SELECT * FROM city;
